cc5dd8860f61086ae142e8d6802f676859fc19e4
acastro23 <alexscastro2002@gmai.com>
2024-10-17 17:05:00 -0400

    AC1017 -- Completed TestScenario2_Test3

--- src/test/java/com/bugbusters/swe_backend/TestScenario2_Test3.java
+++ src/test/java/com/bugbusters/swe_backend/TestScenario2_Test3.java
@@ -1,15 +1,39 @@
 package com.bugbusters.swe_backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+
 public class TestScenario2_Test3 {
 
     /*
-        Inconclusive
+        AC1017 -- Test Scenario 2: User Selects hotel and date for stay. User selects room/rooms. User quits.
+        This test class will test a scenario where a user selects  room type, and then selects available room, and then
+        the room becomes unavailable.
     */
 
+
+    @Test
+    void guestSelectsRoom() {
+
+        String guestChoice = "Single";
+
+        // AC1017 -- Guest selects their type and roomType objects checks if its a valid type
+        String roomType = guestChoice.equals("Single") ? "Single" : "Double";
+        boolean availability = true;
+
+        assertEquals(guestChoice, roomType);
+        assertTrue(availability);
+
+        availability = false;       // AC1017 -- room becomes unavailable after guest books it
+
+        assertFalse(availability);
+    }
+
+
+    @Test
+    void guestQuits() {
+        boolean guestQuits = true;
+        assertTrue(guestQuits);
+    }
 }

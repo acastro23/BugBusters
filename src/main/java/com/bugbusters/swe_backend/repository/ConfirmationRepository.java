8d988b5ea2566574f0e3ea7628cb9b1671045663
ndermer1 <dermernathan@gmail.com>
2024-11-28 12:18:17 -0500

    Test Push using IntlleiJ

--- src/main/java/com/bugbusters/swe_backend/repository/ConfirmationRepository.java
+++ src/main/java/com/bugbusters/swe_backend/repository/ConfirmationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ConfirmationRepository extends JpaRepository<confirmationID, Integer> {
-    
+public interface ConfirmationRepository extends JpaRepository<Confirmation, Long> {
+
 }
--- src/main/java/com/bugbusters/swe_backend/service/ConfirmationService.java
+++ src/main/java/com/bugbusters/swe_backend/service/ConfirmationService.java
@@ -3,7 +3,7 @@ package com.bugbusters.swe_backend.service;
 import com.bugbusters.swe_backend.dto.ConfirmationDTO;
 import com.bugbusters.swe_backend.entity.Confirmation;
 import com.bugbusters.swe_backend.exception.ResourceNotFoundException;
-import com.bugbusters.swe_backend.repository.ConfrimationRepository;
+import com.bugbusters.swe_backend.repository.ConfirmationRepository;
 import org.springframework.stereotype.Service;
 
 public class ConfirmationService {
